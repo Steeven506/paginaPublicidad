364930559e248cebeb2a597cd1cb04ee6b970d7f
Steeven506 <sjchaconm@ufpso.edu.co>
2025-05-24 10:04:49 -0500

    Primer commit del proyecto de publicidad

---
+++ astro.config.mjs
@@ -0,0 +1,23 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import vue from '@astrojs/vue';
+
+import node from '@astrojs/node';
+
+import icon from 'astro-icon';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [vue(), icon()],
+
+  adapter: node({
+    mode: 'standalone'
+  })
+});
